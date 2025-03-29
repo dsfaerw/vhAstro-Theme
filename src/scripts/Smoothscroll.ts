bab79b1daaa36e2ac036980e9c4f11c1eff9b90d
wenxin <96651905+dsfaerw@users.noreply.github.com>
2025-03-29 17:37:08 +0800

    Update Smoothscroll.ts

--- src/scripts/Smoothscroll.ts
+++ src/scripts/Smoothscroll.ts
@@ -3,5 +3,5 @@ import { LoadScript } from "@/utils/index";
 declare const SmoothScroll: any;
 export default async () => {
   await LoadScript("/assets/js/smoothscroll.min.js");
-  SmoothScroll({ stepSize: 120, animationTime: 1000 })
+  SmoothScroll({ stepSize: 80, animationTime: 300 })
 };
